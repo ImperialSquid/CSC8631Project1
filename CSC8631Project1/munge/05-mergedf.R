d5b054622fc8f7feb31b6e42dd0edb665020ec3a
Ben Jaeger <benjaeger99@hgmail.com>
2020-12-11 06:37:07 +0000

    Created merged dfs

--- CSC8631Project1/munge/05-mergedf.R
+++ CSC8631Project1/munge/05-mergedf.R
@@ -3,10 +3,22 @@ for (i in 1:7) {
   qresp_df = get(paste0("qresponses_",i))
   
   new_df = merge(x = enrol_df, y = qresp_df, by.x = "learner_id", by.y = "learner_id")
-  new_df_trim = new_df[colnames(new_df)[c(1,7,8,9,10,11,12,13,14,22)]]
+  new_df_trim = new_df[colnames(new_df)[c(1,7,14,22)]]
+  new_df_trim = cbind(new_df_trim, data.frame(session = i))
   
-  assign(paste0("enrolmentsandresponses_",i), new_df_trim)
+  assign(paste0("enANDqr_",i), new_df_trim)
 }
+
+enANDqr_all = rbind(
+  enANDqr_1,
+  enANDqr_2,
+  enANDqr_3,
+  enANDqr_4,
+  enANDqr_5,
+  enANDqr_6,
+  enANDqr_7
+)
+
 
 rm(enrol_df)
 rm(qresp_df)
