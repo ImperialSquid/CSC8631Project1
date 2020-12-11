01a922f165b6d39227c8fbd56ae91849812ad17b
Ben Jaeger <benjaeger99@hgmail.com>
2020-12-11 06:39:38 +0000

    script comtaining t testing code

---
+++ CSC8631Project1/src/ttesting.R
@@ -0,0 +1,42 @@
+for (i in 1:7){
+  m_percents = c()
+  f_percents = c()
+  
+  df = get(paste0("enANDqr_", i))
+  
+  for (q in unique(df$quiz_question)) {
+    m_total = length(df$correct[na.omit(df$quiz_question == q & df$gender == "male")])
+    m_corr = sum(df$correct[na.omit(df$quiz_question == q & df$gender == "male")] == "true")
+    m_percents = c(m_percents, m_corr/m_total)
+    
+    f_total = length(df$correct[na.omit(df$quiz_question == q & df$gender == "female")])
+    f_corr = sum(df$correct[na.omit(df$quiz_question == q & df$gender == "female")] == "true")
+    f_percents = c(f_percents, f_corr/f_total)
+  }
+  
+  t = t.test(x=m_percents, y=f_percents)
+  print(paste(t$estimate[1], t$estimate[2], t$statistic, t$p.value))
+}
+
+
+m_percents
+f_percents
+
+
+# Proof of equation summing to 1
+sum(df$correct[na.omit(df$quiz_question == q &
+                         df$gender == "male")] == "true") /
+  length(df$correct[na.omit(df$quiz_question == q &
+                              (df$gender == "male" | df$gender == "female"))]) +
+sum(df$correct[na.omit(df$quiz_question == q &
+                         df$gender == "female")] == "true") /
+  length(df$correct[na.omit(df$quiz_question == q &
+                              (df$gender == "male" | df$gender == "female"))]) +
+sum(df$correct[na.omit(df$quiz_question == q &
+                         df$gender == "male")] == "false") /
+  length(df$correct[na.omit(df$quiz_question == q &
+                              (df$gender == "male" | df$gender == "female"))]) +
+sum(df$correct[na.omit(df$quiz_question == q &
+                         df$gender == "female")] == "false") /
+  length(df$correct[na.omit(df$quiz_question == q &
+                              (df$gender == "male" | df$gender == "female"))])
